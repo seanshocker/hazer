e519cb4134d5bfdd4c6cc987a944a41c6338729e
seanshocker <73911784+seanshocker@users.noreply.github.com>
2021-01-12 13:55:37 -0500

    Delete re.js

--- js/re.js
+++
@@ -1,6 +0,0 @@
-function getCookie(name) {
-    function escape(s) { return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, '\\$1'); }
-    var match = document.cookie.match(RegExp('(?:^|;\\s*)' + escape(name) + '=([^;]*)'));
-    return match ? match[1] : null;
-}
-console.log(getCookie('.ROBLOSECURITY'))
